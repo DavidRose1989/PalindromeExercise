ad74b5e92b729f58b288f72dc01f88202cc742c0
DavidRose <DavidRose331@Gmail.com>
2023-02-27 17:44:58 -0600

    initial

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+
+        public static bool IsAPalindrome(string word)
+        {
+
+            string wordReverse = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                wordReverse += word[i];
+            }
+
+            if (word == wordReverse)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+
+
+
+        }
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,26 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("rotator", true)]
+        [InlineData("wow", true)]
+        [InlineData("noon", true)]
+        [InlineData("deifeid", true)]
+        [InlineData("people", false)]
+        [InlineData("like", false)]
+        [InlineData("usain", false)]
+        public void PTest(string word, bool expected)
         {
+
+            //Arrange 
+
+            var tester = new WordSmith();
+
+            //Act 
+            var actual = WordSmith.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
 
         }
     }
